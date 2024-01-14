202e3dd52c89a59383f7ffe75c9be331992b901b
Naveen <naveenkrishna@altgaminng.app>
2024-01-14 15:16:21 +0530

    removed password

--- database/dbconnect.go
+++ database/dbconnect.go
@@ -15,7 +15,7 @@ var (
 )
 
 func GormConnect() {
-	dsn_rd := fmt.Sprintf("host=localhost user=postgres password=Agasthya47@ dbname=remainderTask port=5432 sslmode=disable TimeZone=Asia/Shanghai")
+	dsn_rd := fmt.Sprintf("host=localhost user=postgres password=******* dbname=remainderTask port=5432 sslmode=disable TimeZone=Asia/Shanghai")
 	DBRead, err = gorm.Open(postgres.Open(dsn_rd), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database reader")
